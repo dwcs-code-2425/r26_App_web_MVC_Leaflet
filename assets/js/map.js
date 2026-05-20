c5a9504c88c0c8de0adb740f106e8cb92c1e29dc
dwcs-code-2425 <dwcs.code.2425@gmail.com>
2026-05-20 17:39:12 +0200

    a6.4

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,10 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.min.css';
+import { apiCall } from './api_client.js';
+
+
+
+const BASE_URL = 'https://localhost:8001/api/demo/countries/5';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -12,12 +17,34 @@ L.Icon.Default.mergeOptions({
         'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 });
 
-const map = L.map('map').setView([40, 0], 4);
+const map = L.map('map').setView([40, 0], 0);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap'
 }).addTo(map);
 
-L.marker([40.4168, -3.7038])
-    .addTo(map)
-    .bindPopup("Madrid");
+// L.marker([40.4168, -3.7038])
+//     .addTo(map)
+//     .bindPopup("Madrid");
+
+apiCall(BASE_URL).then(countries => {
+
+    console.log(countries);
+    countries.forEach(country => {
+
+        const marker = L.marker([country.countryLat, country.countryLng])
+
+            .addTo(map);
+
+        marker.bindPopup(`<b>${country.name}</b><br>Población:  ${country.population} hab.`);
+
+
+
+
+    });
+
+}).catch(error => {
+
+    console.error('Error fetching countries:', error);
+
+});
